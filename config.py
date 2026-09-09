54df416c5c48840fcd563ae9cf608c51af9f060b
cetkaj972-stack <cetkaj972@gmail.com>
2026-09-09 22:47:41 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8297733521:AAHKEF2GKS0uzBdlPnMPhI5lrmozBaLoDDE")
+CHANNEL_ID = "@blood_robots_death"
+CHANNEL_URL = "https://t.me/blood_robots_death"
